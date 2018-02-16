85ebbb3688d97dacdd5f9992da99891ca7705e7d
Max Alber <alber.maximilian@gmail.com>
2018-02-16 02:06:30 +0100

    Adding new imagenet tests.

--- innvestigate/utils/tests/networks/imagenet.py
+++ innvestigate/utils/tests/networks/imagenet.py
@@ -15,19 +15,37 @@ import six
 ###############################################################################
 
 
+import keras.applications.resnet50
+import keras.applications.vgg16
+import keras.applications.vgg19
 import keras.layers
 import numpy as np
 
 from . import base
-
+from ...keras import graph as kgraph
 
 __all__ = [
     "vgg16",
-    #"vgg16_all_conv",
+    "vgg19",
 
-    #"caffenet",
-    #"googlenet",
+    #"resnet50",
 ]
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+
+def _prepare_keras_net(clazz, input_shape, output_n, weights=None):
+    model = clazz(weights=None)
+    net = {}
+    net["in"] = model.inputs
+    net["sm_out"] = model.outputs
+    net["out"] = kgraph.pre_softmax_tensors(model.outputs)
+    net["input_shape"] = input_shape
+    net["output_n"] = output_n
+    return net
 
 
 ###############################################################################
@@ -50,140 +68,47 @@ def vgg16_preprocess(X):
     return X
 
 
-def vgg16(activation=None):
-    if activation is None:
-        activation = "relu"
-
-    input_shape = [None, 3, 224, 224]
-    output_n = 1000
-
-    net = {}
-    net["in"] = base.input_layer(shape=input_shape)
-
-    net.update(base.conv_pool(
-        net["in"], 2, "conv_1", 64,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_1_pool"], 2, "conv_2", 128,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_2_pool"], 3, "conv_3", 256,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_3_pool"], 3, "conv_4", 512,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_4_pool"], 3, "conv_5", 512,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-
-    net["conv_flat"] = keras.layers.Flatten()(net["conv_5_pool"])
-    net["dense_1"] = base.dense_layer(net["conv_flat"], units=4096,
-                                      activation=activation,
-                                      kernel_initializer="glorot_uniform")
-    net["dense_1_dropout"] = base.dropout_layer(net["dense_1"], 0.5)
-    net["dense_2"] = base.dense_layer(net["dense_1_dropout"], units=4096,
-                                      activation=activation,
-                                      kernel_initializer="glorot_uniform")
-    net["dense_2_dropout"] = base.dropout_layer(net["dense_2"], 0.5)
-    net["out"] = base.dense_layer(net["dense_2_dropout"], units=output_n,
-                                  kernel_initializer="glorot_uniform")
-    net["sm_out"] = base.softmax(net["out"])
-
-    net.update({
-        "input_shape": input_shape,
-
-        "output_n": output_n,
-    })
-    return net
+def vgg16_preprocess(X):
+    return keras.applications.vgg16.preprocess_input(X)
 
 
-def vgg16_all_conv(activation=None):
-    if activation is None:
-        activation = "relu"
+def vgg16(activation=None, weights=None):
+    return _prepare_keras_net(
+        keras.applications.vgg16.VGG16,
+        [None, 3, 224, 224],
+        1000,
+        weights=weights)
 
-    input_shape = [None, 3, 256, 256]
-    output_n = 1000
-    
-    net = {}
-    net["in"] = base.input_layer(shape=input_shape)
 
-    net.update(base.conv_pool(
-        net["in"], 2, "conv_1", 64,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_1_pool"], 2, "conv_2", 128,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_2_pool"], 3, "conv_3", 256,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_3_pool"], 3, "conv_4", 512,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
-    net.update(base.conv_pool(
-        net["conv_4_pool"], 3, "conv_5", 512,
-        activation=activation,
-        # todo: take care of theano to keras port:
-        # flip_filters=False))
-    ))
+###############################################################################
+###############################################################################
+###############################################################################
 
-    net["dense_1"] = base.conv_layer(net["conv_5_pool"],
-                                     filters=4096, kernel_size=7,
-                                     padding="valid",
-                                     # todo: take care of theano to keras port:
-                                     # flip_filters=False,
-                                     activation=activation,
-                                     kernel_initializer="glorot_uniform")
-    net["dense_1_dropout"] = base.dropout_layer(net["dense_1"], 0.5)
-    net["dense_2"] = base.conv_layer(net["dense_1_dropout"],
-                                     filters=4096, kernel_size=1,
-                                     padding="valid",
-                                     # todo: take care of theano to keras port:
-                                     # flip_filters=False,
-                                     activation=activation,
-                                     kernel_initializer="glorot_uniform")
-    net["dense_2_dropout"] = base.dropout_layer(net["dense_2"], 0.5)
-    net["dense_3"] = base.conv_layer(net["dense_2_dropout"], output_n, 1,
-                                     padding="valid",
-                                     # todo: take care of theano to keras port:
-                                     # flip_filters=False,
-                                     activation=None,
-                                     kernel_initializer="glorot_uniform")
-    # todo: fix:
-    net["global_pool"] = lasagne.layers.GlobalPool(
-        net["dense_3"], pool_function=theano.tensor.sum)
-    net["out"] = lasagne.layers.Activation(
-        net["global_pool"], activation="softmax")
 
-    net.update({
-        "input_shape": input_shape,
+def vgg19_preprocess(X):
+    return keras.applications.vgg19.preprocess_input(X)
 
-        "output_n": output_n,
-    })
-    return net
+
+def vgg19(activation=None, weights=None):
+    return _prepare_keras_net(
+        keras.applications.vgg19.VGG19,
+        [None, 3, 224, 224],
+        1000,
+        weights=weights)
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+
+def resnet50_preprocess(X):
+    return keras.applications.resnet50.preprocess_input(X)
+
+
+def resnet50(activation=None, weights=None):
+    return _prepare_keras_net(
+        keras.applications.resnet50.ResNet50,
+        [None, 3, 224, 224],
+        1000,
+        weights=weights)
