302fd76ff0795fd7defae3946065871a51bfc1f1
Max Alber <alber.maximilian@gmail.com>
2018-02-21 12:16:23 +0100

    Revert vgg16, will switch when both work.

--- innvestigate/utils/tests/networks/imagenet.py
+++ innvestigate/utils/tests/networks/imagenet.py
@@ -83,6 +83,83 @@ def vgg16(activation=None, weights=None):
         weights=weights)
 
 
+VGG16_OFFSET = np.array([103.939, 116.779, 123.68])
+
+
+def vgg16_preprocess(X):
+    if X.shape[1] == 3:
+        shape = [1, 3, 1, 1]
+    else:
+        shape = [1, 1, 1, 3]
+
+    offset = VGG16_OFFSET.reshape(shape)
+    # Remove pixel-wise mean.
+    X -= offset
+    return X
+
+
+def vgg16(activation=None):
+    if activation is None:
+        activation = "relu"
+
+    input_shape = [None, 3, 224, 224]
+    output_n = 1000
+
+    net = {}
+    net["in"] = base.input_layer(shape=input_shape)
+
+    net.update(base.conv_pool(
+        net["in"], 2, "conv_1", 64,
+        activation=activation,
+        # todo: take care of theano to keras port:
+        # flip_filters=False))
+    ))
+    net.update(base.conv_pool(
+        net["conv_1_pool"], 2, "conv_2", 128,
+        activation=activation,
+        # todo: take care of theano to keras port:
+        # flip_filters=False))
+    ))
+    net.update(base.conv_pool(
+        net["conv_2_pool"], 3, "conv_3", 256,
+        activation=activation,
+        # todo: take care of theano to keras port:
+        # flip_filters=False))
+    ))
+    net.update(base.conv_pool(
+        net["conv_3_pool"], 3, "conv_4", 512,
+        activation=activation,
+        # todo: take care of theano to keras port:
+        # flip_filters=False))
+    ))
+    net.update(base.conv_pool(
+        net["conv_4_pool"], 3, "conv_5", 512,
+        activation=activation,
+        # todo: take care of theano to keras port:
+        # flip_filters=False))
+    ))
+
+    net["conv_flat"] = keras.layers.Flatten()(net["conv_5_pool"])
+    net["dense_1"] = base.dense_layer(net["conv_flat"], units=4096,
+                                      activation=activation,
+                                      kernel_initializer="glorot_uniform")
+    net["dense_1_dropout"] = base.dropout_layer(net["dense_1"], 0.5)
+    net["dense_2"] = base.dense_layer(net["dense_1_dropout"], units=4096,
+                                      activation=activation,
+                                      kernel_initializer="glorot_uniform")
+    net["dense_2_dropout"] = base.dropout_layer(net["dense_2"], 0.5)
+    net["out"] = base.dense_layer(net["dense_2_dropout"], units=output_n,
+                                  kernel_initializer="glorot_uniform")
+    net["sm_out"] = base.softmax(net["out"])
+
+    net.update({
+        "input_shape": input_shape,
+
+        "output_n": output_n,
+    })
+    return net
+
+
 ###############################################################################
 ###############################################################################
 ###############################################################################
