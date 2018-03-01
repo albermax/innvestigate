266e8d7072cee2f7a74f2a0097fd5d6b24e3d84b
Philipp Seegerer <philipp.seegerer@tu-berlin.de>
2018-03-01 08:55:52 +0100

    Clean up perturbation.

--- innvestigate/tools/perturbate.py
+++ innvestigate/tools/perturbate.py
@@ -10,32 +10,29 @@ import six
 # End: Python 2/3 compatability header small
 
 import numpy as np
-from heapq import nlargest
+import heapq
 
+from innvestigate.utils.visualizations import batch_flatten
 
 class Perturbation:
-    def __init__(self, analyzer, perturbation_function, steps, recompute_analysis=False):
+    def __init__(self, analyzer, perturbation_function, ratio=0.05, steps=1, reduce_function=np.mean,
+                 recompute_analysis=False):
         self.analyzer = analyzer
         self.perturbation_function = perturbation_function
+        self.ratio = ratio  # How many of the pixels should be perturbated
         self.steps = steps
-
+        self.reduce_function = reduce_function
         self.recompute_analysis = recompute_analysis
 
-        # TODO
         if self.steps != 1:
             raise NotImplementedError("Only 1 perturbation step is supported.")  # TODO
         if self.recompute_analysis:
             raise NotImplementedError("Recomputation of analysis is not implemented yet.")  # TODO
 
-    def calculate_thresholds_on_batch(self, a, nlargest_val, perturbate_largest=True):
+    def calculate_thresholds_on_batch(self, a, num_perturbated_pixels):
         # TODO do not compute threshold but directly the indices (thresholds has advantages, though)
-        def batch_flatten(x):
-            # TODO this could go somewhere else
-            # Flattens all but the first dimensions of a numpy array, i.e. flatten each sample in a batch for Keras tensors
-            return x.reshape(x.shape[0], -1)
-
-        # Sort the values and take the nlargest_val'th entry as threshold  TODO one could probably also directly operate on the indices
-        thresholds = np.array([nlargest(nlargest_val, sample)[-1] for sample in batch_flatten(a)])
+        # Sort the values and take the num_perturbated_pixels'th entry as threshol
+        thresholds = np.array([heapq.nlargest(num_perturbated_pixels, sample)[-1] for sample in batch_flatten(a)])
         return thresholds.reshape(a.shape[0], 1, 1, 1)
 
     def perturbate_on_batch(self, x, perturbation_mask):
@@ -54,19 +51,18 @@ class Perturbation:
         """
         assert a.shape == x.shape, a.shape
         # reduce the analysis along channel axis -> n x 1 x h x w
-        reduce = np.mean  # TODO possible alternatives could be K.max, should be a member variable or user defined
-        a = reduce(a, axis=1, keepdims=True)
+        a = self.reduce_function(a, axis=1, keepdims=True)
         assert a.shape == (x.shape[0], 1, x.shape[2], x.shape[3]), a.shape
 
-        nlargest_val = int(0.05 * x.shape[2] * x.shape[3])  # TODO member variable
-        thresholds = self.calculate_thresholds_on_batch(a,
-                                                        nlargest_val)  # one threshold per sample. If a value is higher than the threshold, it should be perturbated
+        num_perturbated_pixels = int(self.ratio * x.shape[2] * x.shape[3])
+        # Calculate one threshold per sample. If a value is higher than the threshold, it should be perturbated.
+        thresholds = self.calculate_thresholds_on_batch(a, num_perturbated_pixels)
         assert thresholds.shape == (x.shape[0], 1, 1, 1), thresholds.shape
 
         perturbation_mask = a >= thresholds  # mask with ones where the input should be perturbated, zeros otherwise
         assert np.all(np.sum(perturbation_mask, axis=(1, 2,
-                                                      3)) == nlargest_val), "Discrepancy between desired number of perturbations ({}) and actual number of perturbations ({}).".format(
-            nlargest_val, np.sum(perturbation_mask, axis=(1, 2, 3)))
+                                                      3)) == num_perturbated_pixels), "Discrepancy between desired number of perturbations ({}) and actual number of perturbations ({}).".format(
+            num_perturbated_pixels, np.sum(perturbation_mask, axis=(1, 2, 3)))
 
         x_perturbated = self.perturbate_on_batch(x, perturbation_mask)
         return x_perturbated
