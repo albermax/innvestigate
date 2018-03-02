773da95018a50603c2affffb6b84ce0b98e2d687
Philipp Seegerer <philipp.seegerer@tu-berlin.de>
2018-03-02 09:42:37 +0100

    PerturbationAnalysis. Perturbation on generator.

--- examples/mnist_perturbation.py
+++ examples/mnist_perturbation.py
@@ -1,6 +1,6 @@
 # Begin: Python 2/3 compatibility header small
 # Get Python 3 functionality:
-from __future__ import\
+from __future__ import \
     absolute_import, print_function, division, unicode_literals
 from future.utils import raise_with_traceback, raise_from
 # catch exception with: except Exception as e
@@ -37,12 +37,10 @@ import innvestigate.utils as iutils
 import innvestigate.utils.tests.networks.base
 import innvestigate.utils.visualizations as ivis
 
-from innvestigate.tools import Perturbation
-
+from innvestigate.tools import Perturbation, PerturbationAnalysis
 
 base_dir = os.path.dirname(__file__)
 eutils = imp.load_source("utils", os.path.join(base_dir, "utils.py"))
-
 
 keras.backend.set_image_data_format("channels_first")
 
@@ -100,7 +98,7 @@ def train_model(model, data):
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
-    return score
+
 
 ###############################################################################
 ###############################################################################
@@ -110,10 +108,11 @@ if __name__ == "__main__":
 
     zero_mean = False
     pattern_type = "linear"
-    #pattern_type = "relu"
+    # pattern_type = "relu"
     data = fetch_data()
     images = [(data[2][i].copy(), data[3][i]) for i in range(10)]
     label_to_class_name = [str(i) for i in range(10)]
+
 
     ###########################################################################
     # Utility function.
@@ -126,11 +125,13 @@ if __name__ == "__main__":
             X -= 0.5
         return X
 
+
     def postprocess(X):
         X = X.copy()
         X = ivis.postprocess_images(X,
                                     channels_first=False)
         return X
+
 
     def image(X):
         X = X.copy()
@@ -139,14 +140,18 @@ if __name__ == "__main__":
         return ivis.graymap(X,
                             input_is_postive_only=True)
 
+
     def bk_proj(X):
         return ivis.graymap(X)
+
 
     def heatmap(X):
         return ivis.heatmap(X)
 
+
     def graymap(X):
         return ivis.graymap(np.abs(X), input_is_postive_only=True)
+
 
     ###########################################################################
     # Build model.
@@ -154,7 +159,7 @@ if __name__ == "__main__":
     data_preprocessed = (preprocess(data[0]), data[1],
                          preprocess(data[2]), data[3])
     model, modelp = create_model()
-    score = train_model(modelp, data_preprocessed)
+    train_model(modelp, data_preprocessed)
     model.set_weights(modelp.get_weights())
 
     ###########################################################################
@@ -170,22 +175,17 @@ if __name__ == "__main__":
     analyzer.fit(data_preprocessed[0], pattern_type=pattern_type,
                  batch_size=256, verbose=0)
     # Create analysis.
-    x_test, y_test = data_preprocessed[2:]
-    x_test_perturbated = x_test  # TODO inplace?
-    batch_size = 256
-    current_index = 0
-    while current_index < x_test.shape[0]:
-        if current_index + batch_size < x_test.shape[0]:
-            curr_batch_size = batch_size
-        else:
-            current_batch_size = x_test.shape[0] - current_index
-        image = x_test[current_index:current_index + curr_batch_size]
-        # Predict label.
-        x = preprocess(image)
-        perturbation = Perturbation(analyzer, perturbation_function, steps=1)
-        a = analyzer.analyze(x)
-        x_perturbated = perturbation.compute_on_batch(x, a)
-        current_index += batch_size
     num_classes = 10
-    score_perturbated = modelp.evaluate(x_test_perturbated, keras.utils.to_categorical(y_test, num_classes), verbose=0)
-    print("score {}, score_perturbated {}".format(score, score_perturbated))
+    batch_size = 256
+
+    # Data loading
+    x_test, y_test = data_preprocessed[2:]
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    generator = iutils.BatchSequence([x_test, y_test], batch_size=batch_size)
+
+    x_test_perturbated = x_test  # TODO inplace?
+    current_index = 0
+    perturbation = Perturbation(analyzer, perturbation_function, ratio=0.01)
+    perturbation_analysis = PerturbationAnalysis(analyzer, modelp, generator, perturbation, preprocess)
+    scores = perturbation_analysis.compute_perturbation_analysis()
+    print(scores)
