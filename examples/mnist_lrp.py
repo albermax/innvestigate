c93c206465098cc5d6cb4efdc3f9a7f2e1d303f5
Sebastian Roland Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2018-04-09 11:42:34 +0200

    reactivating lrpmethods using input_layer_rule

--- examples/mnist_lrp.py
+++ examples/mnist_lrp.py
@@ -222,9 +222,9 @@ if __name__ == "__main__":
         ("lrp.z_plus",            {},                       heatmap, "LRP-ZPlus"),
         ("lrp.z_plus_fast",       {},                       heatmap, "LRP-ZPlusFast"),
         ("lrp.composite_a",           {},                     heatmap, "LRP-CompositeA"),
-        #("lrp.composite_a_flat",      {},                     heatmap, "LRP-CompositeAFlat"),
+        ("lrp.composite_a_flat",      {},                     heatmap, "LRP-CompositeAFlat"),
         ("lrp.composite_b",           {},                     heatmap, "LRP-CompositeB"),
-        #("lrp.composite_b_flat",      {},                     heatmap, "LRP-CompositeBFlat"),
+        ("lrp.composite_b_flat",      {},                     heatmap, "LRP-CompositeBFlat"),
     ]
 
     # Create analyzers.
