981f63f70d203f11ad817c769606e14376689372
Sebastian Roland Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2018-04-12 20:34:53 +0200

    all layers should be covered now. running tests.

---
+++ examples/run_everything.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+for net in vgg16 vgg19 resnet50 inception_v3 inception_resnet_v2 densenet121 densenet169 densenet201 nasnet_large nasnet_mobile ; do
+for script in all_methods.py imagenet_lrp.py ; do
+    logfile=./logs/$net-$script.txt
+    echo running $script. writing output to $logfile
+    python3 $script >> $logfile
+    echo ""
+done
+done
+echo "run_everything.sh has terminated."
