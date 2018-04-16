d27cbf2967404d1033ede5d1df346abe06008d60
Maximilian Alber <work.alber.maximilian@gmail.com>
2018-04-16 04:34:07 +0200

    Adding VGG16/VGG19 tests for pattern-based methods.

--- innvestigate/tests/analyzer/test_pattern_based.py
+++ innvestigate/tests/analyzer/test_pattern_based.py
@@ -60,6 +60,22 @@ def test_precommit__PatternNet():
     dryrun.test_analyzer(method, "mnist.*")
 
 
+@pytest.mark.slow
+@pytest.mark.application
+@pytest.mark.imagenet
+def test_imagenet__PatternNet():
+
+    def method(model):
+        # enough for test purposes, only pattern application is tested here
+        # pattern computation is tested separately.
+        # assume that one dim weights are biases, drop them.
+        patterns = [x for x in model.get_weights()
+                    if len(x.shape) > 1]
+        return PatternNet(model, patterns=patterns)
+
+    dryrun.test_analyzer(method, "imagenet.vgg16:imagenet.vgg19")
+
+
 @pytest.mark.fast
 @pytest.mark.precommit
 def test_fast__PatternAttribution():
@@ -87,6 +103,22 @@ def test_precommit__PatternAttribution():
         return PatternAttribution(model, patterns=patterns)
 
     dryrun.test_analyzer(method, "mnist.*")
+
+
+@pytest.mark.slow
+@pytest.mark.application
+@pytest.mark.imagenet
+def test_imagenet__PatternAttribution():
+
+    def method(model):
+        # enough for test purposes, only pattern application is tested here
+        # pattern computation is tested separately.
+        # assume that one dim weights are biases, drop them.
+        patterns = [x for x in model.get_weights()
+                    if len(x.shape) > 1]
+        return PatternAttribution(model, patterns=patterns)
+
+    dryrun.test_analyzer(method, "imagenet.vgg16:imagenet.vgg19")
 
 
 ###############################################################################
