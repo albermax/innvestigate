f5e70123af4cbf5df759028619f219c61923d433
Sebastian Roland Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2018-04-16 20:05:21 +0200

    test runs again

--- examples/run_everything.sh
+++ examples/run_everything.sh
@@ -5,11 +5,14 @@ if [ ! -d "./logs" ]; then
 fi
 
 for script in all_methods.py imagenet_lrp.py ; do
-#for net in vgg16 vgg19 resnet50 inception_v3 inception_resnet_v2 densenet121 densenet169 densenet201 nasnet_large nasnet_mobile ; do
-for net in nasnet_mobile nasnet_large ; do
+for net in vgg16 vgg19 resnet50 inception_v3 inception_resnet_v2 densenet121 densenet169 densenet201 nasnet_large nasnet_mobile ; do
+#for net in nasnet_mobile nasnet_large ; do
+    t_start=$(date)
     logfile=./logs/$net-$script.txt
     echo running $script. writing output to $logfile
     python3 $script $net >> $logfile
+    t_end=$(date)
+    echo $t_start "->" $t_end >>$logfile
     echo ""
 done
 done
