2b2204eb4692bc88ec301770ea11bad1fecffeb4
Sebastian Roland Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2018-04-16 19:27:24 +0200

    dirty quick fix reverting line which breaks everything for imagenet.

--- innvestigate/applications/imagenet.py
+++ innvestigate/applications/imagenet.py
@@ -142,7 +142,8 @@ def _prepare_keras_net(clazz, image_shape,
     weights = None
     if load_weights is True:
         weights = "imagenet"
-    model = clazz(weights=weights, input_shape=net["input_shape"][1:])
+    #model = clazz(weights=weights, input_shape=net["input_shape"][1:])
+    model = clazz(weights=weights)
     net["model"] = model
 
     net["in"] = model.inputs
