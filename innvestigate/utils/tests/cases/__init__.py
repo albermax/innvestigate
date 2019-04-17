f377d6dcde9b8b53f2536d422d8fcf01baf55e6e
Max Alber <alber.maximilian@gmail.com>
2019-04-17 18:39:54 +0200

    Add additional convience functions for marking test cases.

--- innvestigate/utils/tests/cases/__init__.py
+++ innvestigate/utils/tests/cases/__init__.py
@@ -56,19 +56,50 @@ PRECOMMIT = [
 ]
 
 
-def mark_as_xfail(case_ids, xfails):
-    """Mark cases as expected failures.
+def _mark_cases(case_ids, to_mark, mark):
+    """Mark cases.
 
-    :param case_ids: Parameter list for pytest.mar.parametrize.
-    :param xfails: Parameters in case_ids to mark as expected failures.
+    :param case_ids: Parameter list for pytest.mark.parametrize.
+    :param xfails: List of parameters in case_ids to mark.
+    :param mark: Mark to apply.
     :return: case_ids with added marks.
     """
     ret = []
     for case in case_ids:
-        if case in xfails:
+        if case in to_mark:
             if not isinstance(case, tuple):
                 case = (case,)
             # Mark as expected failure
-            case = pytest.param(*case, marks=pytest.mark.xfail)
+            case = pytest.param(*case, marks=mark)
         ret.append(case)
     return ret
+
+
+def mark_as_xfail(case_ids, xfails):
+    """Mark cases as expected failures.
+
+    :param case_ids: Parameter list for pytest.mark.parametrize.
+    :param xfails: List of parameters in case_ids to mark as expected failures.
+    :return: case_ids with added marks.
+    """
+    return _mark_cases(case_ids, xfails, pytest.mark.xfail)
+
+
+def mark_as_skip(case_ids, skips):
+    """Mark cases to skip.
+
+    :param case_ids: Parameter list for pytest.mark.parametrize.
+    :param xfails: List of parameters in case_ids to mark for skip.
+    :return: case_ids with added marks.
+    """
+    return _mark_cases(case_ids, xfails, pytest.mark.skip)
+
+
+def filter(case_ids, to_filter):
+    """Filter cases.
+
+    :param case_ids: Parameter list for pytest.mark.parametrize.
+    :param to_filter: List of parameters to filter from case_ids.
+    :return: Filtered case_ids.
+    """
+    return [case for case in case_ids if case not in to_filter]
