28011abdd3355f7677ca21afa24de25448e9e296
Leander Weber <leander.weber@hhi.fraunhofer.de>
2020-10-15 17:50:44 +0200

    fixed small mistake

--- weber@172.17.25.141/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from . import analyzer
-from .analyzer import create_analyzer
-from .analyzer import NotAnalyzeableModelException
-
-# Disable pyflaks warnings:
-assert analyzer
-assert create_analyzer
-assert NotAnalyzeableModelException
--- weber@172.17.25.141/tools/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .pattern import PatternComputer
-from .perturbate import Perturbation
-from .perturbate import PerturbationAnalysis
-
-# Make pylint ignore the imports
-assert PatternComputer
-assert Perturbation
-assert PerturbationAnalysis
