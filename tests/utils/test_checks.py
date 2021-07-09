c8b359ffac497719a75343b2edc3c64d14642bfd
Adrian Hill <adrian.hill@mailbox.org>
2021-07-09 11:45:21 +0200

    Add tests for checks of activation layers and `only_relu_activation`

--- tests/utils/test_checks.py
+++ tests/utils/test_checks.py
@@ -7,18 +7,55 @@ import innvestigate.utils.keras.checks as ichecks
 @pytest.mark.fast
 @pytest.mark.precommit
 @pytest.mark.parametrize(
-    "activation,expected",
+    "activation,layer,expected",
     [
-        ("selu", False),
-        ("relu", True),
-        ("softmax", False),
-        ("linear", False),
-        (None, False),
+        ("relu", keras.layers.Dense(5, activation="relu"), True),
+        ("linear", keras.layers.Dense(5, activation="linear"), True),
+        ("none", keras.layers.Dense(5, activation=None), True),  # defaults to linear
+        ("selu", keras.layers.Dense(5, activation="selu"), False),
+        ("softmax", keras.layers.Dense(5, activation="softmax"), False),
+        ("relu", keras.layers.ReLU(), True),
+        ("elu", keras.layers.ELU(), False),
     ],
 )
-def test_contains_activation_relu(activation, expected):
-    layer = keras.layers.Dense(5, activation=activation)
+def test_only_relu_activation(activation, layer, expected):
+    assert ichecks.only_relu_activation(layer) == expected
+
+
+@pytest.mark.fast
+@pytest.mark.precommit
+@pytest.mark.parametrize(
+    "activation,layer,expected",
+    [
+        ("selu", keras.layers.Dense(5, activation="selu"), False),
+        ("relu", keras.layers.Dense(5, activation="relu"), True),
+        ("softmax", keras.layers.Dense(5, activation="softmax"), False),
+        ("linear", keras.layers.Dense(5, activation="linear"), False),
+        ("relu", keras.layers.ReLU(), True),
+        ("elu", keras.layers.ELU(), False),
+        ("none", keras.layers.Dense(5, activation=None), False),  # defaults to linear
+    ],
+)
+def test_contains_activation_relu(activation, layer, expected):
     assert ichecks.contains_activation(layer, "relu") == expected
+
+
+@pytest.mark.fast
+@pytest.mark.precommit
+@pytest.mark.parametrize(
+    "activation,layer,expected",
+    [
+        ("elu", keras.layers.Dense(5, activation="elu"), True),
+        ("relu", keras.layers.Dense(5, activation="relu"), False),
+        ("softmax", keras.layers.Dense(5, activation="softmax"), False),
+        ("linear", keras.layers.Dense(5, activation="linear"), False),
+        ("relu", keras.layers.ReLU(), False),
+        ("elu", keras.layers.ELU(), True),
+        ("none", keras.layers.Dense(5, activation=None), False),  # defaults to linear
+    ],
+)
+def test_contains_activation_elu(activation, layer, expected):
+    assert ichecks.contains_activation(layer, "elu") == expected
 
 
 @pytest.mark.fast
