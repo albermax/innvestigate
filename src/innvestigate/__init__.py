7946a12510580d8c10680d7e921329b47f6b6115
Adrian Hill <adrian.hill@mailbox.org>
2022-02-24 12:15:03 +0100

    Update major version number

--- src/innvestigate/__init__.py
+++ src/innvestigate/__init__.py
@@ -3,4 +3,4 @@ from innvestigate.analyzer import create_analyzer  # noqa
 from innvestigate.analyzer.base import NotAnalyzeableModelException  # noqa
 from innvestigate.backend.graph import model_wo_softmax  # noqa
 
-__version__ = "1.0.10"
+__version__ = "2.0.0"
