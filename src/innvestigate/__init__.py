48e1cd5c28f5670afc7344a4df27874e13580e54
Ruben Lucas <ruben.lucas1997@gmail.com>
2023-02-13 09:14:29 +0100

    Add support for different output activations

--- src/innvestigate/__init__.py
+++ src/innvestigate/__init__.py
@@ -1,6 +1,6 @@
 from innvestigate import analyzer  # noqa
 from innvestigate.analyzer import create_analyzer  # noqa
 from innvestigate.analyzer.base import NotAnalyzeableModelException  # noqa
-from innvestigate.backend.graph import model_wo_softmax  # noqa
+from innvestigate.backend.graph import model_wo_output_activation  # noqa
 
 __version__ = "2.0.1"
