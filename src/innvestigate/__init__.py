4c0f35577641bfc5a2872a480c10e803a6bda1df
Ruben <50332377+Rubinjo@users.noreply.github.com>
2023-05-05 18:36:29 +0200

    Add way to strip arbitrary output activations (#310)

--- src/innvestigate/__init__.py
+++ src/innvestigate/__init__.py
@@ -1,6 +1,9 @@
 from innvestigate import analyzer  # noqa
 from innvestigate.analyzer import create_analyzer  # noqa
 from innvestigate.analyzer.base import NotAnalyzeableModelException  # noqa
-from innvestigate.backend.graph import model_wo_softmax  # noqa
+from innvestigate.backend.graph import (
+    model_wo_softmax,
+    model_wo_output_activation,
+)  # noqa
 
 __version__ = "2.0.1"
