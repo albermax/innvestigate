37aae3e034a69a5cffa34fa627bed2487b4174eb
Adrian Hill <adrian.hill@mailbox.org>
2023-05-05 19:15:19 +0200

    Fix import to pass CI

--- src/innvestigate/__init__.py
+++ src/innvestigate/__init__.py
@@ -1,9 +1,9 @@
 from innvestigate import analyzer  # noqa
 from innvestigate.analyzer import create_analyzer  # noqa
 from innvestigate.analyzer.base import NotAnalyzeableModelException  # noqa
-from innvestigate.backend.graph import (
-    model_wo_softmax,
+from innvestigate.backend.graph import (  # noqa
     model_wo_output_activation,
-)  # noqa
+    model_wo_softmax,
+)
 
 __version__ = "2.0.1"
